2ad38b90dc03a0295a4c96413fdbfaef3dd8d402
roberto.urbani <thetenebrax23@gmail.com>
2020-09-07 11:31:56 +0200

    First commit: created plugin

---
+++ mc-main.php
@@ -0,0 +1,13 @@
+<?php
+/*
+  Plugin Name: Barcode generator
+  Plugin URI:
+  Description: Generates products barcode
+  Author: ITRO Team(c)
+  E-mail: info@itroteam.com
+  Version: 1.0
+  Author URI: http://www.itroteam.com/
+  Text Domain: itro_ipp
+  License: GPLv2 or later
+  License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
